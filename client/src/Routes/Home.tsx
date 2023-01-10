ae8f095c631a209996086c320f03e3e1ba2374e8
MathiasBln <53523648+MathiasBln@users.noreply.github.com>
2023-01-10 11:13:50 +0100

    change color logout

--- client/src/Routes/Home.tsx
+++ client/src/Routes/Home.tsx
@@ -1,7 +1,7 @@
 import {IShowProps} from "../types/Post";
 import FormPost from "../Component/FormPost";
 import ShowPost from "../Component/ShowPost";
-import {useLocation, useNavigate} from "react-router-dom";
+import { useNavigate} from "react-router-dom";
 import React from "react";
 
 export default function Home({setPosts, posts}: IShowProps) {
@@ -15,7 +15,7 @@ export default function Home({setPosts, posts}: IShowProps) {
 
     return(
         <>
-            <button className="btn btn-outline-light btn-lg px-5" onClick={deco}>Logout</button>
+            <button className="btn btn-outline-dark btn-lg px-5" onClick={deco}>Logout</button>
             <FormPost setPosts={setPosts} posts={posts}/>
             <ShowPost setPosts={setPosts} posts={posts}/>
         </>
