f300199d57c963abdee700861a467bf272cc7155
MathiasBln <53523648+MathiasBln@users.noreply.github.com>
2023-01-10 10:35:53 +0100

    change BDD

--- client/src/Routes/Login.tsx
+++ client/src/Routes/Login.tsx
@@ -53,7 +53,7 @@ export default function Form() {
     }
 
     return (
-            <section className="min-vh-100 gradient-custom">
+            <section className="min-vh-100">
                 <div className="container py-5 h-100">
                     <div className="row d-flex justify-content-center align-items-center h-100">
                         <div className="col-12 col-md-8 col-lg-6 col-xl-5">
