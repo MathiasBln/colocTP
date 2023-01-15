4ea4ac4e3625f0f489c1bbed9397639f2db4cc56
Amaury Franssen <amauryfranssen@gmail.com>
2023-01-15 11:00:13 +0100

    création d'une route racine pour aller à home et donc au login au moment du npm start

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -33,7 +33,7 @@ function App() {
             <BrowserRouter>
                 <Routes>
                     <Route path={"/login"} element={<Login/>}/>
-                    <Route path={'/home'} element={
+                    <Route path={'/'} element={
                         <NeedAuth>
                             <Home setColoc={setColoc} coloc={coloc}/>
                         </NeedAuth>
