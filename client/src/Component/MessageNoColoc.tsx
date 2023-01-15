8ea1c5e250998c2dfa70173f5c80032321323bdf
Amaury Franssen <amauryfranssen@gmail.com>
2023-01-15 03:35:57 +0100

    fix(MessageColoc): correction de fautes, renommer les fichiers NavBar.. au lieu de navBar...

--- client/src/Component/MessageNoColoc.tsx
+++ client/src/Component/MessageNoColoc.tsx
@@ -26,14 +26,12 @@ return(
         <div className="fw-bold mb-2 mt-2">
         <p className="fs-4 mt-2">Bonjour, nous vous informons que vous n'avez pas ou plus accés à l'espace de gestion.<br/>
         Nous vous invitons à écrire un email à l'une des colocations mentionnés en page d'accueil pour tenter une admission afin de devenir colocataire.
-        Le fait d'être colocataire donne accés à l'espace de gestion de votre colocation.<br/>
-        Il est également possible qu'une mesure de sécurité ou un incident technique vous ait retiré votre statut de colocataire sur le site. 
-        Dans ce cas il vous suffit de vous déconnecter et de vous reconnecter.
-        </p>
-
+        Le fait d'être colocataire donne accés à l'espace de gestion de votre colocation.</p>
+        <p className="fs-4">Il est également possible qu'une mesure de sécurité ou un incident technique vous ait retiré votre statut de colocataire sur le site. 
+        Dans ce cas il vous suffit de vous déconnecter et de vous reconnecter.</p>
         <p className="fs-4">Si vous vous sentez l'âme d'un entrepreneur ou que vous avez déjà de l'immobilier, vous pouvez créer votre colocation via le formulaire.
         Vous devenez alors votre premier colocataire en cumulant cela avec le statut d'administrateur de la colocation. 
-        Vous avez alors la capacité de gestion étendue des colocataires</p>
+        Vous avez alors la capacité de gestion étendue des colocataires.</p>
         </div>
 
         <div className="row align-content-center justify-content-center gap-2 pb-5"> 
